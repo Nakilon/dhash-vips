07bf5d896b7fcb8c61b5e640e00f64ab211eb62d
Victor Maslov <nakilon@gmail.com>
2017-09-25 11:32:02 +0300

    indentation

--- lib/dhash-vips.rb
+++ lib/dhash-vips.rb
@@ -4,26 +4,26 @@ require "vips"
 module DHashVips
   module DHash
 
-  extend self
+    extend self
 
-  def hamming a, b
-    (a ^ b).to_s(2).count "1"
-  end
-
-  def pixelate file, hash_size, kernel = nil
-    image = Vips::Image.new_from_file file
-    if kernel
-      image.resize((hash_size + 1).fdiv(image.width), vscale: hash_size.fdiv(image.height), kernel: kernel).colourspace("b-w")
-    else
-      image.resize((hash_size + 1).fdiv(image.width), vscale: hash_size.fdiv(image.height)                ).colourspace("b-w")
+    def hamming a, b
+      (a ^ b).to_s(2).count "1"
     end
-  end
 
-  def calculate file, hash_size = 8, kernel = nil
-    image = pixelate file, hash_size, kernel
+    def pixelate file, hash_size, kernel = nil
+      image = Vips::Image.new_from_file file
+      if kernel
+        image.resize((hash_size + 1).fdiv(image.width), vscale: hash_size.fdiv(image.height), kernel: kernel).colourspace("b-w")
+      else
+        image.resize((hash_size + 1).fdiv(image.width), vscale: hash_size.fdiv(image.height)                ).colourspace("b-w")
+      end
+    end
 
-    image.cast("int").conv([1, -1]).crop(1, 0, 8, 8).>(0)./(255).cast("uchar").to_a.join.to_i(2)
-  end
+    def calculate file, hash_size = 8, kernel = nil
+      image = pixelate file, hash_size, kernel
+
+      image.cast("int").conv([1, -1]).crop(1, 0, 8, 8).>(0)./(255).cast("uchar").to_a.join.to_i(2)
+    end
 
   end
 
