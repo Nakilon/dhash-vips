207aa1a4eaea9c72f1fd8221722769ae64bf9b81
Victor Maslov <nakilon@gmail.com>
2019-12-03 08:25:07 +0300

    renamed post-install test file; using a single .sh script to report compilation problems

---
+++ report_an_error.sh
@@ -0,0 +1,4 @@
+echo "Failed to build native extensions for #distance3 method: $1."
+echo "If you are using rbenv then install Ruby with -k flag."
+echo "If you are using something else then consider contributing."
+echo "This is not an error though -- the #distance3 method will fallback to pure Ruby implementation."
