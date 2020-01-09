e2fd929d3894ecf5d09520211ce0783c17bb2b3c
Victor Maslov <nakilon@gmail.com>
2020-01-09 21:46:06 +0300

    draft backup branch switch commit

--- example_ocr/temp.rb
+++ example_ocr/temp.rb
@@ -1,16 +1,23 @@
 require "dhash-vips"
 
 # Courier Menlo Monaco Tahoma
-chars = %w{
-  Arial Verdana
-}.flat_map do |font|
+# Arial Verdana
+# chars = %w{
+#   DIN\ Condensed
+#   Tahoma Arial
+# }.flat_map do |font|
+chars = [
+  ["DIN Condensed, Bold", "/Library/Fonts/DIN Condensed Bold.ttf"],
+].flat_map do |font, fontfile|
   FileUtils.mkdir_p "chars/#{font}"
   (?A..?Z).map do |char|
     filename = "chars/#{font}/#{char.ord}.png"
     Vips::Image.text(char, font: font, width: 100, height: 100).invert.write_to_file filename unless File.exist? filename
+    # Vips::Image.text(char, fontfile: fontfile, font: font, width: 100, height: 100).invert.write_to_file filename unless File.exist? filename
     [DHashVips::IDHash.fingerprint(filename), char]
   end
 end
+exit
 
 
 unless File.exist? "7dtd-menu.png"
@@ -21,8 +28,8 @@ image = Vips::Image.new_from_file("7dtd-menu.png").colourspace("b-w").invert
 
 otsu = lambda do |image|
   require "unicode_plot"
-  # UnicodePlot.barplot((0..25).map(&:to_s), (image / 10).hist_find.to_a.flatten, width: 80).render STDOUT
-  # puts
+  UnicodePlot.barplot((0..25).map(&:to_s), (image / 10).hist_find.to_a.flatten, width: 80).render STDOUT
+  puts
   histData = image.hist_find.to_a.flatten
   total = histData.sum
   sum = 0
@@ -51,34 +58,42 @@ otsu = lambda do |image|
   p threshold
 end
 bw = image.>(otsu[image]).ifthenelse(255, 0)
-bw.write_to_file "temp1.png"
+bw.write_to_file "tempbw.png"
 require "byebug"
 # image.to_a[a..b]
 # trim = threshold.find_trim threshold: 0, background: [255]
 # p trim
 # image = threshold.crop(*trim)
-split = lambda do |array|
-  array.each_with_index.chunk{ |row,| row.any?{ |c,| c < 255 } }.select(&:first).map do |_, chunk|
+split_v = lambda do |image|
+  image.invert.profile[1].to_a.each_with_index.chunk{ |row,| row != [[image.width]] }.select(&:first).map do |_, chunk|
     t = chunk.map(&:last)
-    [chunk.map(&:first), t[0], t[-1]]
+    a, b = t[0], t[-1]
+    [image.crop(0, a, image.width, b - a + 1), a, b]
   end
 end
-split[bw.to_a].each do |chunk, a, b|
+split_h = lambda do |image|
+  image.invert.profile[0].to_a[0].each_with_index.chunk{ |row,| row != [image.height] }.select(&:first).map do |_, chunk|
+    t = chunk.map(&:last)
+    a, b = t[0], t[-1]
+    [image.crop(a, 0, b - a + 1, image.height), a, b]
+  end
+end
+split_v[bw].each_with_index do |(chunk, a, b), i|
   require "profile" if ENV["PROFILE"]
   # p [a, b]
-  # splitted = split[bw.crop(0, a, image.width, b - a + 1).to_a.transpose]
-  splitted = split[chunk.transpose]
+  # splitted = split_v[bw.crop(0, a, image.width, b - a + 1).to_a.transpose]
+  splitted = split_h[chunk]
   c, d = splitted[0][1], splitted[-1][2]
   p [a, b, c, d]
   t = image.>(otsu[image.crop(c, a, d - c + 1, b - a + 1)]).ifthenelse(255, 0)
-  t.crop(c, a, d - c + 1, b - a + 1).write_to_file "temp2.png"
-# byebug
-  chunk, a, b = split[t.to_a].find{ |_, aa, bb| aa <= a && bb >= b || aa >= a && bb <= b }
+  image.crop(c, a, d - c + 1, b - a + 1).write_to_file "temp.png"
+  t.crop(c, a, d - c + 1, b - a + 1).write_to_file "temp#{i}.png"
+  chunk, a, b = split_v[t].find{ |_, aa, bb| aa <= a && bb >= b || aa >= a && bb <= b }
   fail unless chunk
-  # splitted = split[t.crop(0, a, image.width, b - a + 1).to_a.transpose]
-  split[chunk.transpose].map do |chunk, c, d|
-    image.crop(c, a, d - c + 1, b - a + 1).write_to_file "temp3.png"
-    Vips::Image.new_from_array(chunk.transpose).write_to_file "temp4.png"
+  # splitted = split_h[t.crop(0, a, image.width, b - a + 1).to_a.transpose]
+  split_h[chunk].map do |chunk, c, d|
+    # image.crop(c, a, d - c + 1, b - a + 1).write_to_file "temp3.png"
+    # chunk.write_to_file "temp4.png"
     # exit
     # p [a, b]
     # exit
@@ -89,7 +104,7 @@ split[bw.to_a].each do |chunk, a, b|
     fingerprint = begin
       # image.crop(a, 0, b - a + 1, image.height).write_to_file "temp.png"
       temp.write image.crop(c, a, d - c + 1, b - a + 1).write_to_buffer(".png")
-      FileUtils.copy temp.tap(&:rewind).path, "temp.png"
+      # FileUtils.copy temp.tap(&:rewind).path, "temp#{i}.png"
 # exit
       DHashVips::IDHash.fingerprint temp.tap(&:rewind).path
     ensure
