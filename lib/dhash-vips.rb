e0d1d8a64f1d52ed18e075b1d4059c0e86c275ef
Victor Maslov <nakilon@gmail.com>
2022-04-18 14:25:44 +0300

    redefined median as a method instead of lambda var

--- lib/dhash-vips.rb
+++ lib/dhash-vips.rb
@@ -21,16 +21,14 @@ module DHashVips
 
     def calculate file, hash_size = 8, kernel = nil
       image = pixelate file, hash_size, kernel
-
       image.cast("int").conv([[1, -1]]).crop(1, 0, hash_size, hash_size).>(0)./(255).cast("uchar").to_a.join.to_i(2)
     end
 
   end
 
   module IDHash
-    extend self
 
-    def distance3_ruby a, b
+    def self.distance3_ruby a, b
       ((a ^ b) & (a | b) >> 128).to_s(2).count "1"
     end
     begin
@@ -40,7 +38,7 @@ module DHashVips
     else
       # we can't just do `defined? Bignum` because it's defined but deprecated (some internal CONST_DEPRECATED flag)
       if Gem::Version.new(RUBY_VERSION) < Gem::Version.new("2.4")
-        def distance3 a, b
+        def self.distance3 a, b
           if a.is_a?(Bignum) && b.is_a?(Bignum)
             distance3_c a, b
           else
@@ -51,7 +49,7 @@ module DHashVips
         # https://github.com/ruby/ruby/commit/de2f7416d2deb4166d78638a41037cb550d64484#diff-16b196bc6bfe8fba63951420f843cfb4R10
         require "rbconfig/sizeof"
         FIXNUM_MAX = (1 << (8 * RbConfig::SIZEOF["long"] - 2)) - 1
-        def distance3 a, b
+        def self.distance3 a, b
           if a > FIXNUM_MAX && b > FIXNUM_MAX
             distance3_c a, b
           else
@@ -60,7 +58,7 @@ module DHashVips
         end
       end
     end
-    def distance a, b
+    def self.distance a, b
       size_a, size_b = [a, b].map do |x|
         # TODO write a test about possible hash sizes
         #      they were 32 and 128, 124, 120 for MRI 2.0
@@ -72,7 +70,7 @@ module DHashVips
       ((a ^ b) & (a | b) >> 2 * size_a * size_a).to_s(2).count "1"
     end
 
-    @median = lambda do |array|
+    def self.median array
       h = array.size / 2
       return array[h] if array[h] != array[h - 1]
       right = array.dup
@@ -82,17 +80,18 @@ module DHashVips
       return right.uniq[1] if left.count(left.last) > right.count(right.first)
       left.last
     end
-    fail unless 2 == @median[[1, 2, 2, 2, 2, 2, 3]]
-    fail unless 3 == @median[[1, 2, 2, 2, 2, 3, 3]]
-    fail unless 3 == @median[[1, 1, 2, 2, 3, 3, 3]]
-    fail unless 2 == @median[[1, 1, 1, 2, 3, 3, 3]]
-    fail unless 2 == @median[[1, 1, 2, 2, 2, 2, 3]]
-    fail unless 2 == @median[[1, 2, 2, 2, 2, 3]]
-    fail unless 3 == @median[[1, 2, 2, 3, 3, 3]]
-    fail unless 1 == @median[[1, 1, 1]]
-    fail unless 1 == @median[[1, 1]]
+    fail unless 2 == median([1, 2, 2, 2, 2, 2, 3])
+    fail unless 3 == median([1, 2, 2, 2, 2, 3, 3])
+    fail unless 3 == median([1, 1, 2, 2, 3, 3, 3])
+    fail unless 2 == median([1, 1, 1, 2, 3, 3, 3])
+    fail unless 2 == median([1, 1, 2, 2, 2, 2, 3])
+    fail unless 2 == median([1, 2, 2, 2, 2, 3])
+    fail unless 3 == median([1, 2, 2, 3, 3, 3])
+    fail unless 1 == median([1, 1, 1])
+    fail unless 1 == median([1, 1])
+    private_class_method :median
 
-    def fingerprint filename, power = 3
+    def self.fingerprint filename, power = 3
       size = 2 ** power
       image = Vips::Image.new_from_file filename, access: :sequential
       image = image.resize(size.fdiv(image.width), vscale: size.fdiv(image.height)).colourspace("b-w").flatten
@@ -100,7 +99,7 @@ module DHashVips
       array = image.to_enum.map &:flatten
       d1, i1, d2, i2 = [array, array.transpose].flat_map do |a|
         d = a.zip(a.rotate(1)).flat_map{ |r1, r2| r1.zip(r2).map{ |i, j| i - j } }
-        m = @median.call d.map(&:abs).sort
+        m = median d.map(&:abs).sort
         [
           d.map{ |c| c     <  0 ? 1 : 0 }.join.to_i(2),
           d.map{ |c| c.abs >= m ? 1 : 0 }.join.to_i(2),
